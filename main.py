478df062adf701d03ac22b9cf9a6e8e30be74db2
Geeks-Vegeta <shreyasmohite786@gmail.com>
2022-10-25 18:31:07 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,67 @@
+from scrap import ScrapBlogs
+import re
+import requests
+from bs4 import BeautifulSoup
+from deta import Deta
+from dotenv import load_dotenv, find_dotenv
+import io
+import os
+import datetime
+
+load_dotenv(find_dotenv())
+
+
+urls = os.environ.get("URL")
+project_key = os.environ.get("PROJECTKEY")
+current_date = datetime.datetime.now()
+folder_date=current_date.strftime("%x")
+new_folder_date=folder_date.replace("/", "_")
+
+
+deta = Deta(project_key)
+urls = ScrapBlogs(urls)
+
+folder_name = f"mediumblogs{new_folder_date}"
+drive = deta.Drive(folder_name)
+
+
+
+def main():
+
+    try:
+
+        for url in urls:
+
+            scrap_url = f"https://medium.com{url}"
+            find_name=re.findall('\w+', url)
+            name_list=[]
+            for x in find_name:
+                if x.isalpha():
+                    name_list.append(x)
+
+            file_name="_".join(name_list)
+
+            response = requests.get(scrap_url).text.encode('utf8').decode('ascii', 'ignore')
+            soup = BeautifulSoup(response, 'html.parser')
+            find_section = soup.find("section")
+            if find_section is None:
+                continue
+            else:
+                with io.StringIO() as f:
+                    f.write(f"{scrap_url}")
+                    f.write("\n")
+                    f.write(str(find_section))
+
+                    drive.put(f"{file_name}.md", f.getvalue())
+
+        return "Scrapped Successfully"
+
+
+    except Exception as e:
+        print(e)
+    
+
+
+if __name__ == "__main__":
+    main()
+    
---
+++ scrap.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+
+
+def ScrapBlogs(urls):
+    
+    response = requests.get(str(urls)).text.encode('utf8').decode('ascii', 'ignore')
+    soup = BeautifulSoup(response, 'html.parser')
+    find_all_a = soup.find_all("a")
+    urls=set()
+
+    for x in find_all_a:
+        if 'https' not in  x['href'] and \
+            "/tag/" not in x['href'] and \
+            "/?source" not in x['href'] and \
+            "/plans?" not in x['href'] and \
+            not re.match("/@\w*[a-zA-Z0-9]\?", x['href']):
+            urls.add(x['href'])
+
+    url_list = list(urls)
+    return url_list[1:10]
+
